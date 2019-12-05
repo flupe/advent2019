3ab0387798f3f2684103198f1683b82213c884bb
flupe <lucas@escot.me>
2019-12-05 02:46:03 +0100

    improved day3

--- day3.hs
+++ day3.hs
@@ -14,8 +14,8 @@ main = do
         (horiz1, verts1) = getLines wire1
         (horiz2, verts2) = getLines wire2
 
-        intersections = mapMaybe intersection ([ (h, v) | h <- horiz1, v <- verts2 ]
-                                            ++ [ (h, v) | h <- horiz2, v <- verts1 ])
+        intersections = filter intersecting ([ (h, v) | h <- horiz1, v <- verts2 ]
+                                          ++ [ (h, v) | h <- horiz2, v <- verts1 ])
 
     print $ smallest day1 intersections
     print $ smallest day2 intersections
@@ -24,12 +24,11 @@ main = do
         toCmd (x : xs) = (read [x], read xs)
         getLines = computeLines 0 (0, 0) ([], [])
         inRange x a b = a <= x && x <= b
-        intersection a@(E yH x1 x2 _ _, E xV y1 y2 _ _)
-            | inRange yH y1 y2 && inRange xV x1 x2 = Just a
-            | otherwise = Nothing
+        intersecting (E yH x1 x2 _ _, E xV y1 y2 _ _) = inRange yH y1 y2 && inRange xV x1 x2
         smallest f = head . sort . map f
 
 day1 (E yH _ _ _ _, E xV _ _ _ _) = abs xV + abs yH
+
 day2 (E yH x1 x2 s1 d1, E xV y1 y2 s2 d2) =
     d1 + d2 + delta s1 xV x1 x2 + delta s2 yH y1 y2
     where delta Up x x1 x2 = x - x1
@@ -43,4 +42,3 @@ computeLines dist (x, y) (horiz, verts) ((d, i):xs) =
         R -> comp (x + i, y) (E y x (x + i) Up dist : horiz, verts) xs
         L -> comp (x - i, y) (E y (x - i) x Dn dist : horiz, verts) xs
     where comp = computeLines (dist + i)
-
